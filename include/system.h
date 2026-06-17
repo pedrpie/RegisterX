e3c3da97026745fbb0bb03d2dd7c6229bd0fa830
pedrpie1 <eupedrohenrique100@gmail.com>
2026-06-17 19:24:23 -0300

    padronizacao

---
+++ include/system.h
@@ -0,0 +1,9 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+void cadastrar();
+void listar();
+void atualizar();
+void deletar();
+
+#endif
---
+++ src/system.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../include/system.h";
+
+using namespace std;
+
+void cadastrar(){
+
+}
+
+void listar(){
+    
+}
+
+void atualizar(){
+    
+}
+
+void deletar(){
+    
+}
