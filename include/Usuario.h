b24dfcfc640dd26c1d26dc66672d113a6d17fc29
Codexgsn <gustavopower2007@gmail.com>
2026-06-17 19:40:08 -0300

    Criação do struct Usuario

---
+++ include/Usuario.h
@@ -0,0 +1,16 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+#include <string>
+
+struct Usuario {
+
+    int id;
+    std::string nome;
+    int idade;
+    std::string mail;
+    int cpf;
+    std::string curso;
+
+};
+
+#endif
